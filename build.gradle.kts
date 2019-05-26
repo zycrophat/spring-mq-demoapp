9c243b62560355bff3f325d2f6611bfc8894812f
Andreas Steffan <andreas.steffan@mailbox.org>
2019-05-26 16:21:39 +0200

    changed version

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "steffan"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 java.targetCompatibility = JavaVersion.VERSION_1_8
 
