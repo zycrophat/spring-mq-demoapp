0cb660145daa4678b51e844523551161e8311f0f
Andreas Steffan <andreas.steffan@mailbox.org>
2019-05-26 15:16:01 +0200

    added idempotent consumer functionality

--- src/main/kotlin/steffan/springmqdemoapp/routes/GreetingRouteBuilder.kt
+++ src/main/kotlin/steffan/springmqdemoapp/routes/GreetingRouteBuilder.kt
@@ -2,8 +2,14 @@ package steffan.springmqdemoapp.routes
 
 import org.apache.camel.CamelContext
 import org.apache.camel.builder.RouteBuilder
+import org.apache.camel.builder.xml.Namespaces
+import org.apache.camel.processor.idempotent.FileIdempotentRepository
+import org.apache.camel.processor.idempotent.MemoryIdempotentRepository
 import org.apache.camel.spi.DataFormat
+import org.apache.camel.util.AsyncProcessorHelper.process
 import org.springframework.stereotype.Component
+import steffan.springmqdemoapp.api.bindings.GreetingRequest
+import java.io.File
 
 
 @Component
@@ -19,11 +25,23 @@ class GreetingRouteBuilder(
 
         from("jms:greetConvert")
                 .transacted()
+                .idempotentConsumer()
+                    .xpath("//g:greetingRequest/g:name/text()",
+                            Namespaces("g", "urn:steffan.springmqdemoapp:greeting")
+                    )
+                    .messageIdRepository(
+                            FileIdempotentRepository.fileIdempotentRepository(File("run/greetConvertDedup"))
+                    )
                 .process(typeConvertingGreetingRequestProcessor)
 
         from("jms:greetUnmarshall")
                 .transacted()
                 .unmarshal(jaxbDataFormat)
+                .idempotentConsumer()
+                    .body(GreetingRequest::class.java, GreetingRequest::getName)
+                    .messageIdRepository(
+                            FileIdempotentRepository.fileIdempotentRepository(File("run/greetUnmarshallDedup"))
+                    )
                 .process(unmarshalledGreetingRequestProcessor)
     }
 }
