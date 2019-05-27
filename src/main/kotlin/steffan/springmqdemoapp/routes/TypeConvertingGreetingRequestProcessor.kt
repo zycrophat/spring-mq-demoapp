a00e454992524e39e5b61996aa63137d0faff0b5
Andreas Steffan <andreas.steffan@mailbox.org>
2019-05-27 21:37:31 +0200

    added isolation level to annotations

--- src/main/kotlin/steffan/springmqdemoapp/routes/TypeConvertingGreetingRequestProcessor.kt
+++ src/main/kotlin/steffan/springmqdemoapp/routes/TypeConvertingGreetingRequestProcessor.kt
@@ -4,6 +4,8 @@ import org.apache.camel.Exchange
 import org.apache.camel.Processor
 import org.apache.camel.TypeConverter
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Isolation
+import org.springframework.transaction.annotation.Transactional
 import steffan.springmqdemoapp.api.bindings.GreetingRequest
 import steffan.springmqdemoapp.util.Logging
 import steffan.springmqdemoapp.util.logger
@@ -12,6 +14,7 @@ import steffan.springmqdemoapp.util.logger
 @Component
 class TypeConvertingGreetingRequestProcessor(private val typeConverter: TypeConverter) : Processor, Logging {
 
+    @Transactional(isolation = Isolation.SERIALIZABLE)
     override fun process(exchange: Exchange) {
         val request = typeConverter
                 .convertTo(GreetingRequest::class.java, exchange, exchange.getIn().body)
--- src/main/kotlin/steffan/springmqdemoapp/routes/UnmarshalledGreetingRequestProcessor.kt
+++ src/main/kotlin/steffan/springmqdemoapp/routes/UnmarshalledGreetingRequestProcessor.kt
@@ -3,6 +3,8 @@ package steffan.springmqdemoapp.routes
 import org.apache.camel.Exchange
 import org.apache.camel.Processor
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Isolation
+import org.springframework.transaction.annotation.Transactional
 import steffan.springmqdemoapp.api.bindings.GreetingRequest
 import steffan.springmqdemoapp.util.Logging
 import steffan.springmqdemoapp.util.logger
@@ -11,6 +13,7 @@ import steffan.springmqdemoapp.util.logger
 @Component
 class UnmarshalledGreetingRequestProcessor : Processor, Logging {
 
+    @Transactional(isolation = Isolation.SERIALIZABLE)
     override fun process(exchange: Exchange) {
         val request = exchange.getIn().getBody(GreetingRequest::class.java)
 
