8893894a122ae145bb8d7e2c8579ac97949ac52f
Andreas Steffan <andreas.steffan@mailbox.org>
2019-05-30 17:18:27 +0200

    optimized versioning rule for non-master branch and detached head commits

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,25 +9,36 @@ plugins {
 }
 
 gitVersion.rules {
-    val versionPattern = """v?(\d+)\.(\d+)\.(\d+)""".toPattern()
-
+    val minorVersionPattern = """v?(\d+)\.(\d+)\.(0)""".toPattern()
+    val patchVersionPattern = """v?(\d+)\.(\d+)\.(\d+)""".toPattern()
+    
     before {
-        val tag = findLatestTag(versionPattern)
+        val tag = findLatestTag(minorVersionPattern)
         version.major = tag?.matches?.getAt(1)?.toInt() ?: 0
         version.minor = tag?.matches?.getAt(2)?.toInt() ?: 0
-        version.patch = countCommitsSince(tag as HasObjectId, true)
     }
 
     onBranch("master") {
-        val tag = findLatestTag(versionPattern)
-        if (countCommitsSince(tag as HasObjectId) != 0) {
+        val latestMinorVersionTag = findLatestTag(minorVersionPattern)
+        val countCommitsSinceLatestMinorVersionTag = countCommitsSince(latestMinorVersionTag as HasObjectId, true)
+        val latestPatchVersionTag = findLatestTag(patchVersionPattern)
+        val latestPatch = latestPatchVersionTag?.matches?.getAt(3)?.toInt() ?: 0
+        version.patch = countCommitsSinceLatestMinorVersionTag
+        
+        version.patch = countCommitsSinceLatestMinorVersionTag
+        if (countCommitsSinceLatestMinorVersionTag != latestPatch) {
             version.setPrereleaseTag("SNAPSHOT")
         }
         isSkipOtherRules = true
     }
-
+    
     always {
-        version.setPrereleaseTag(branchName)
+        val tag = findLatestTag(patchVersionPattern)
+        version.patch = tag?.matches?.getAt(3)?.toInt() ?: 0
+
+        val label = if ((branchName ?: "HEAD") != "HEAD") branchName else head?.id(6)
+        val countCommitsSinceTag = countCommitsSince(tag as HasObjectId, true)
+        version.setPrereleaseTag("${countCommitsSinceTag}.${label}")
     }
 }
 
