8d61a8b4bea0813f0b85d83df2cebeefc9079e9f
Andreas Steffan <andreas.steffan@mailbox.org>
2019-11-13 20:59:12 +0100

    downgraded spring-boot and spring-framework versions for SBA compatibility

--- buildSrc/src/main/kotlin/LibraryVersions.kt
+++ buildSrc/src/main/kotlin/LibraryVersions.kt
@@ -1,7 +1,7 @@
 object LibraryVersions {
-    const val SPRING_FRAMEWORK_VERSION = "5.2.1.RELEASE"
+    const val SPRING_FRAMEWORK_VERSION = "5.1.11.RELEASE"
     const val KOTLIN_VERSION = "1.3.50"
-    const val SPRING_BOOT_BASE_VERSION = "2.2.1"
+    const val SPRING_BOOT_BASE_VERSION = "2.1.9"
     const val SPRING_BOOT_ADMIN_VERSION = "2.1.6"
     const val SPRING_BOOT_VERSION = "$SPRING_BOOT_BASE_VERSION.RELEASE"
     const val CAMEL_VERSION = "2.24.2"
