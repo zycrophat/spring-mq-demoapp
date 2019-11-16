472d75312a5afed6f0144cce4e4d544e750ce173
Andreas Steffan <andreas.steffan@mailbox.org>
2019-11-16 19:36:26 +0100

    upgraded to winsw 2.3.0

--- buildSrc/src/main/kotlin/LibraryVersions.kt
+++ buildSrc/src/main/kotlin/LibraryVersions.kt
@@ -6,7 +6,7 @@ object LibraryVersions {
     const val SPRING_BOOT_VERSION = "$SPRING_BOOT_BASE_VERSION.RELEASE"
     const val CAMEL_VERSION = "2.24.2"
     const val JAXB_VERSION = "2.3.2"
-    const val WINSW_VERSION = "2.2.0:bin@exe"
+    const val WINSW_VERSION = "2.3.0:bin@exe"
     const val JACKSON_VERSION = "2.10.0+"
     const val JASYPT_SPRING_BOOT_VERSION = "2.1.2"
 }
