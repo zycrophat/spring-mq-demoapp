551fe30b719bd2d34668a24781afcda6cc0e0957
Andreas Steffan <andreas.steffan@mailbox.org>
2019-12-10 19:26:29 +0100

    upgraded Spring versions

--- buildSrc/src/main/kotlin/LibraryVersions.kt
+++ buildSrc/src/main/kotlin/LibraryVersions.kt
@@ -1,8 +1,8 @@
 object LibraryVersions {
-    const val SPRING_FRAMEWORK_VERSION = "5.1.11.RELEASE"
+    const val SPRING_FRAMEWORK_VERSION = "5.2.2.RELEASE"
     const val KOTLIN_VERSION = "1.3.61"
-    const val SPRING_BOOT_BASE_VERSION = "2.1.9"
-    const val SPRING_BOOT_ADMIN_VERSION = "2.1.6"
+    const val SPRING_BOOT_BASE_VERSION = "2.2.2"
+    const val SPRING_BOOT_ADMIN_VERSION = "2.2.0"
     const val SPRING_BOOT_VERSION = "$SPRING_BOOT_BASE_VERSION.RELEASE"
     const val CAMEL_VERSION = "2.24.2"
     const val JAXB_VERSION = "2.3.2"
